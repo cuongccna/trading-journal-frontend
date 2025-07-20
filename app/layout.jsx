3c22ff85a2de25a4ecc194433d5d20a1b7db2f23
cuongccna <cuongccna@users.noreply.github.com>
2025-07-20 12:37:55 +0700

    Fix missing Providers import

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,7 +1,6 @@
 
 "use client";
-import { Provider } from "react-redux";
-import { store } from "@/store/store";
+import Providers from "../components/Providers";
 
 export default function RootLayout({ children }) {
   return (
